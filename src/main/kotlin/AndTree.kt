263a7a5317ef4f11063cd47afefa158c30591456
Devon Hockley <devon@hockley.ca>
2019-11-21 16:04:17 -0700

    did search process class, needs looking at.

--- src/main/kotlin/AndTree.kt
+++ src/main/kotlin/AndTree.kt
@@ -2,24 +2,35 @@ import java.lang.Exception
 
 abstract class AndTree<T>(root: T) {
 
+    // equivalent to:
+    // private final MutableList<Node> _leaves = mutableListOf(new Node(root))
     private val _leaves = mutableListOf(Node(root))
+
+
 
     // read only view of _leaves (does not copy)
     val leaves: kotlin.collections.List<Node> get() = _leaves
 
     abstract fun childGen(pred: T) : kotlin.collections.List<T>
 
-    inner class Node(val value: T,private val _children: MutableList<Node> = mutableListOf()){
+    open inner class Node(val value: T, private val _children: MutableList<Node> = mutableListOf()){
+
+        var solved: Boolean = false
 
         // read-only view of _children (does not copy)
         val children: List<Node> get() = _children
 
         fun expand(){
-            if (_children.isEmpty()) {
+            if (_children.isEmpty() && !solved) {
                 childGen(value).forEach {
                     val x = Node(it)
                     _children.add(x)
                     _leaves.add(x)
+                }
+                if(_children.isEmpty()){
+                    solved = true
+                }else{
+                    _leaves.remove(this)
                 }
             }else{
                 throw Exception("Non-empty children")
--- src/main/kotlin/DataClass/PSol.kt
+++ src/main/kotlin/DataClass/PSol.kt
@@ -3,9 +3,13 @@ package DataClass
 // makes the class struct-like.
 data class PSol(val assignments: List<Assignment>) {
 
+
     // creates ocerloaded function so java coded can use the default parameters.
     @JvmOverloads
     // creates new copy of the pSol that has one item reassigned.
     // .minus().plus() creates new copy with 1 element replaced.
-    fun copyEdit(new: Assignment, original: Assignment = Assignment(new.course, null)) : PSol = PSol(assignments.minus(original).plus(new))
+
+    fun copyEdit(new: Assignment, original: Assignment = Assignment(new.course, null)) : PSol {
+        return PSol(assignments.minus(original).plus(new))
+    }
 }
