7edd15e5be172bb39fecd3e7db5eda554175719a
Zahra <zahra.ghavasieh@ucalgary.ca>
2019-11-21 16:29:53 -0700

    initial commit

---
+++ src/main/java/Constr.java
@@ -0,0 +1,20 @@
+import DataClass.Assignment;
+import DataClass.PSol;
+
+public class Constr {
+
+    //Attributes
+
+    //Constructors
+    public Constr(){
+
+    }
+
+    //Functions
+    public boolean constr(PSol pr){
+
+        return true;
+    }
+
+
+}
---
+++ src/main/java/Eval.java
@@ -0,0 +1,19 @@
+import DataClass.Assignment;
+import DataClass.PSol;
+
+public class Eval {
+
+    //Attributes
+
+    //Constructors
+    public Eval(){
+
+    }
+
+    //Functions
+    public int eval(PSol pr){
+
+        return 0;
+    }
+
+}
