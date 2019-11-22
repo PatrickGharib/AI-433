67614058eb68efd864756ecf95ad120e9f02fad9
PatrickGharib <patrick.a.gharib@gmail.com>
2019-11-22 01:00:00 -0700

    parsing is finished, need to apply object creation

--- src/Parser.java
+++ src/Parser.java
@@ -33,7 +33,7 @@ public class Parser {
                 if (line.trim().matches("Course slots:") && !zonesRead.contains(line.trim()) && zonesRead.size() == 1) {
                     zonesRead.add(line.trim());
                     System.out.println(zonesRead.toString());
-                    line = readCourseSlot(br,line);
+                    line = readCourseSlot(br, line);
 
                 } // System.exit(0);
 
@@ -52,30 +52,37 @@ public class Parser {
                 if (line.trim().matches("Labs:") && !zonesRead.contains(line.trim()) && zonesRead.size() == 4) {
                     zonesRead.add(line.trim());
                     System.out.println(zonesRead.toString());
+                    line = readLab(br, line);
                 }
                 if (line.trim().matches("Not compatible:") && !zonesRead.contains(line.trim()) && zonesRead.size() == 5) {
                     zonesRead.add(line.trim());
                     System.out.println(zonesRead.toString());
+                    line = readNotCompatible(br, line);
                 }
 
                 if (line.trim().matches("Unwanted:") && !zonesRead.contains(line.trim()) && zonesRead.size() == 6) {
                     zonesRead.add(line.trim());
                     System.out.println(zonesRead.toString());
+                    line = readUnwanted(br, line);
+
                 }
 
                 if (line.trim().matches("Preferences:") && !zonesRead.contains(line.trim()) && zonesRead.size() == 7) {
                     zonesRead.add(line.trim());
                     System.out.println(zonesRead.toString());
+                    line = readPreferences(br, line);
                 }
 
                 if (line.trim().matches("Pair:") && !zonesRead.contains(line.trim()) && zonesRead.size() == 8) {
                     zonesRead.add(line.trim());
                     System.out.println(zonesRead.toString());
+                    line = readPair(br, line);
                 }
 
                 if (line.trim().matches("Partial assignments:") && !zonesRead.contains(line.trim()) && zonesRead.size() == 9) {
                     zonesRead.add(line.trim());
                     System.out.println(zonesRead.toString());
+                    readPartialAssignment(br,line);
                 }
 
 
@@ -91,110 +98,343 @@ public class Parser {
         }
     }
 
-    private static String readCourseSlot(BufferedReader br, String line) {
-        String lastLine = line;
-        String exitString = "Lab slots:";
-        try {
-            while(!(line = br.readLine()).trim().matches(exitString)){
-                if (line.matches("[\\s]*")) {lastLine = line; continue;}
-                if (!line.matches(RegexStrings.COURSE_SLOTS)){
-                    System.out.println("Parsing error: Could not parse File in course slot");
-                    System.out.println(line);
-                    System.exit(0);
-                }
-                if (line.matches(RegexStrings.COURSE_SLOTS)){
-                    System.out.println(line);
-                }
-
-
-                lastLine = line;
-            }
-            if (!lastLine.matches("[\\s]*"))
-                {System.out.println("Parsing error: Could not parse File in course slot(no space)");System.exit(0); }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return line;
-    }
-    private static String readLabSlot(BufferedReader br, String line) {
-        String lastLine = line;
-        String exitString = "Courses:";
-        try {
-            while(!(line = br.readLine()).trim().matches(exitString)){
-                if (line.matches("[\\s]*")) {lastLine = line; continue;}
-                if (!line.matches(RegexStrings.LAB_SLOTS)){
-                    System.out.println("Parsing error: Could not parse File in Lab slot");
-                    System.out.println(line);
-                    System.exit(0);
-                }
-                if (line.matches(RegexStrings.LAB_SLOTS)){
-                    System.out.println(line);
-                }
-
-
-                lastLine = line;
-            }
-            if (!lastLine.matches("[\\s]*"))
-                {System.out.println("Parsing error: Could not parse File in Lab slot(no space)");System.exit(0); }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return line;
-    }
-    private static String readCourse(BufferedReader br, String line) {
-        String lastLine = line;
-        String exitString = "Labs:";
-        try {
-            while(!(line = br.readLine()).trim().matches(exitString)){
-                if (line.matches("[\\s]*")) {lastLine = line; continue;}
-                if (!line.matches(RegexStrings.COURSES)){
-                    System.out.println("Parsing error: Could not parse File in Courses");
-                    System.out.println(line);
-                    System.exit(0);
-                }
-                if (line.matches(RegexStrings.COURSES)){
-                    System.out.println(line);
-                }
-
-
-                lastLine = line;
-            }
-            if (!lastLine.matches("[\\s]*"))
-                {System.out.println("Parsing error: Could not parse File in Courses(no space)");System.exit(0); }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return line;
-    }
-
     private static String readName(BufferedReader br, String line) {
         try {
             int namesFound = 0;
             String lastLine = "";
             String exitString = "Course slots:";
             while (!(line = br.readLine()).trim().matches(exitString)) {
-                if (line.matches("[\\s]*")) {lastLine = line; continue;}
+                if (line.matches("[\\s]*")) {
+                    lastLine = line;
+                    continue;
+                }
                 if (line.matches("[\\s]*[\\S]+[\\s]*")) namesFound++;
-                else {System.out.println("Parsing error: Could not parse File in Name");/*System.exit(0);*/}
+                else {
+                    System.out.println("Parsing error: Could not parse File in Name");/*System.exit(0);*/
+                }
                 lastLine = line;
             }
-            if (!lastLine.matches("[\\s]*") || namesFound != 1)
-                {System.out.println("Parsing error: Could not parse File in Name(no space)");/*System.exit(0);*/ }
+            if (!lastLine.matches("[\\s]*") || namesFound != 1) {
+                System.out.println("Parsing error: Could not parse File in Name(no space)");/*System.exit(0);*/
+            }
         } catch (IOException e) {
-            e.printStackTrace();
-        } catch (NullPointerException e) {
             e.printStackTrace();
         }
         return line;
     }
 
+    private static String readCourseSlot(BufferedReader br, String line) {
+        String lastLine = line;
+        String exitString = "Lab slots:";
+        try {
+            while (!(line = br.readLine()).trim().matches(exitString)) {
+                if (line.matches("[\\s]*")) {
+                    lastLine = line;
+                    continue;
+                }
+                if (!line.matches(RegexStrings.COURSE_SLOTS)) {
+                    System.out.println("Parsing error: Could not parse File in course slot");
+                    System.out.println(line);
+                    System.exit(0);
+                }
+                if (line.matches(RegexStrings.COURSE_SLOTS)) {
+                    System.out.println(line);
+                }
+
+
+                lastLine = line;
+            }
+            if (!lastLine.matches("[\\s]*")) {
+                System.out.println("Parsing error: Could not parse File in course slot(no space)");
+                System.exit(0);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return line;
+    }
+
+    private static String readLabSlot(BufferedReader br, String line) {
+        String lastLine = line;
+        String exitString = "Courses:";
+        try {
+            while (!(line = br.readLine()).trim().matches(exitString)) {
+                if (line.matches("[\\s]*")) {
+                    lastLine = line;
+                    continue;
+                }
+                if (!line.matches(RegexStrings.LAB_SLOTS)) {
+                    System.out.println("Parsing error: Could not parse File in Lab slot");
+                    System.out.println(line);
+                    System.exit(0);
+                }
+                if (line.matches(RegexStrings.LAB_SLOTS)) {
+                    System.out.println(line);
+                }
+
+
+                lastLine = line;
+            }
+            if (!lastLine.matches("[\\s]*")) {
+                System.out.println("Parsing error: Could not parse File in Lab slot(no space)");
+                System.exit(0);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return line;
+    }
+
+    private static String readCourse(BufferedReader br, String line) {
+        String lastLine = line;
+        String exitString = "Labs:";
+        try {
+            while (!(line = br.readLine()).trim().matches(exitString)) {
+                if (line.matches("[\\s]*")) {
+                    lastLine = line;
+                    continue;
+                }
+                if (!line.matches(RegexStrings.COURSES)) {
+                    System.out.println("Parsing error: Could not parse File in Courses");
+                    System.out.println(line);
+                    System.exit(0);
+                }
+                if (line.matches(RegexStrings.COURSES)) {
+                    System.out.println(line);
+                }
+
+
+                lastLine = line;
+            }
+            if (!lastLine.matches("[\\s]*")) {
+                System.out.println("Parsing error: Could not parse File in Courses(no space)");
+                System.exit(0);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return line;
+    }
+
+    private static String readLab(BufferedReader br, String line) {
+        String lastLine = line;
+        String exitString = "Not compatible:";
+        try {
+            while (!(line = br.readLine()).trim().matches(exitString)) {
+                if (line.matches("[\\s]*")) {
+                    lastLine = line;
+                    continue;
+                }
+                if (!line.matches(RegexStrings.LABS) && !line.matches(RegexStrings.TUT)) {
+                    System.out.println("Parsing error: Could not parse File in labs");
+                    System.out.println(line);
+                    System.exit(0);
+                }
+                if (line.matches(RegexStrings.LABS)) {
+                    System.out.println(line);
+                }
+                if (line.matches(RegexStrings.TUT)) {
+                    System.out.println(line);
+                }
+
+
+                lastLine = line;
+            }
+            if (!lastLine.matches("[\\s]*")) {
+                System.out.println("Parsing error: Could not parse File in labs(no space)");
+                System.exit(0);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return line;
+    }
+
+    private static String readNotCompatible(BufferedReader br, String line) {
+        String lastLine = line;
+        String exitString = "Unwanted:";
+        try {
+            while (!(line = br.readLine()).trim().matches(exitString)) {
+                if (line.matches("[\\s]*")) {
+                    lastLine = line;
+                    continue;
+                }
+                if (!line.matches(RegexStrings.NOTCOMPATABLE_FORMAT)) {
+                    System.out.println("Parsing error: Could not parse File in Not Compatible");
+                    System.out.println(line);
+                }
+                if (line.matches(RegexStrings.NOTCOMPATABLE_FORMAT)) {
+                    Matcher matcher = Pattern.compile(RegexStrings.NOTCOMPATABLE_FORMAT).matcher(line);
+                    String[] splitUnwanted = line.split(",");
+                    for (String x : splitUnwanted) {
+                        if (x.matches(".*(TUT|LAB).*")) {
+                            if (x.matches(RegexStrings.LABS)) System.out.println(x);
+                            else if (x.matches(RegexStrings.TUT)) System.out.println(x);
+                        } else System.out.println(x);
+                    }
+                }
+                lastLine = line;
+            }
+            if (!lastLine.matches("[\\s]*")) {
+                System.out.println("Parsing error: Could not parse File in Not Compatible (no space)");
+                System.exit(0);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return line;
+    }
+
+    private static String readUnwanted(BufferedReader br, String line) {
+        String lastLine = line;
+        String exitString = "Preferences:";
+        try {
+            while (!(line = br.readLine()).trim().matches(exitString)) {
+                //System.out.println(line);
+                if (line.matches("[\\s]*")) {
+                    lastLine = line;
+                    continue;
+                }
+                if(!line.matches(RegexStrings.UNWANTED)){
+                    System.out.println("Parsing error: Could not parse File in UnWanted");
+                    System.out.println(line);
+                }
+                if (line.matches(RegexStrings.UNWANTED)) {
+                    Matcher matcher = Pattern.compile(RegexStrings.UNWANTED).matcher(line);
+                    if (line.matches(".*(TUT|LAB).*")) {
+                        if (line.matches(RegexStrings.UNWANTED_L)) System.out.println(line + " LAB");
+                        else if (line.matches(RegexStrings.UNWANTED_T)) System.out.println(line + " TUT");
+                    } else System.out.println(line+ " COURSE");
+                }
+                lastLine = line;
+            }
+            if (!lastLine.matches("[\\s]*")) {
+                System.out.println("Parsing error: Could not parse File in Unwanted(no space)");
+                System.exit(0);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return line;
+    }
+
+    private static String readPreferences(BufferedReader br, String line) {
+        String lastLine = line;
+        String exitString = "Pair:";
+        try {
+            while (!(line = br.readLine()).trim().matches(exitString)) {
+                //System.out.println(line);
+                if (line.matches("[\\s]*")) {
+                    lastLine = line;
+                    continue;
+                }
+                if(!line.matches(RegexStrings.PREFERENCES)){
+                    System.out.println("Parsing error: Could not parse File in Preferences");
+                    System.out.println(line);
+                }
+                if (line.matches(RegexStrings.PREFERENCES)) {
+                    Matcher matcher = Pattern.compile(RegexStrings.PREFERENCES).matcher(line);
+                    if (line.matches(".*(TUT|LAB).*")) {
+                        if (line.matches(RegexStrings.PREFERENCES_L)) System.out.println(line + " LAB");
+                        else if (line.matches(RegexStrings.PREFERENCES_T)) System.out.println(line + " TUT");
+                    } else if (line.matches(RegexStrings.PREFERENCES_C)) System.out.println(line + " COURSE");
+                }
+                lastLine = line;
+            }
+            //System.out.println(RegexStrings.PREFERENCES);
+            if (!lastLine.matches("[\\s]*")) {
+                System.out.println("Parsing error: Could not parse File in Preferences(no space)");
+                System.exit(0);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return line;
+    }
+
+    private static String readPair(BufferedReader br, String line) {
+        String lastLine = line;
+        String exitString = "Partial assignments:";
+        try {
+            while (!(line = br.readLine()).trim().matches(exitString)) {
+                if (line.matches("[\\s]*")) {
+                    lastLine = line;
+                    continue;
+                }
+                if (!line.matches(RegexStrings.PAIR)){
+                    System.out.println("Parsing error: Could not parse File in Pair");
+                    System.out.println(line);
+                }
+                if (line.matches(RegexStrings.PAIR)) {
+                    Matcher matcher = Pattern.compile(RegexStrings.PAIR).matcher(line);
+                    String[] splitUnwanted = line.split(",");
+                    for (String x : splitUnwanted) {
+                        if (x.matches(".*(TUT|LAB).*")) {
+                            if (x.matches(RegexStrings.LABS)) System.out.println(x);
+                            else if (x.matches(RegexStrings.TUT)) System.out.println(x);
+                        } else System.out.println(x);
+                    }
+                }
+                lastLine = line;
+            }
+            if (!lastLine.matches("[\\s]*")) {
+                System.out.println("Parsing error: Could not parse File in Pair(no space)");
+                System.exit(0);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return line;
+    }
+
+    private static void readPartialAssignment(BufferedReader br, String line) {
+        String lastLine = line;
+        try {
+            while (!((line = br.readLine()) == null)) {
+                if (line.matches("[\\s]*")) {
+                    lastLine = line;
+                    continue;
+                }
+                if (!line.matches(RegexStrings.UNWANTED)) {
+                    System.out.println("Parsing error: Could not parse File in Partial Assignment ");
+                    System.out.println(line);
+                }
+                if (line.matches(RegexStrings.UNWANTED)) {
+                    Matcher matcher = Pattern.compile(RegexStrings.UNWANTED).matcher(line);
+                    if (line.matches(".*(TUT|LAB).*")) {
+                        if (line.matches(RegexStrings.UNWANTED_L)) System.out.println(line + " LAB");
+                        else if (line.matches(RegexStrings.UNWANTED_T)) System.out.println(line + " TUT");
+                    } else System.out.println(line + " COURSE");
+                }
+                lastLine = line;
+            }
+//            if (!lastLine.matches("[\\s]*")) {
+//                System.out.println("Parsing error: Could not parse File in Partial Assignment(no space)");
+//                System.exit(0);
+//            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
     public static void main(String[] args) {
         //String fileName = args[0];
         //Scanner in = new Scanner(System.in);String fileName = in.nextLine();
-        for (String x : args){
+        for (String x : args) {
 
             inputReader(x);
             System.out.println("_______________________________");
--- src/RegexStrings.java
+++ src/RegexStrings.java
@@ -10,30 +10,35 @@ public class RegexStrings {
 //    String LEC_VALID = ("[\\s]*(LEC)[\\s]*");
 //    String TUT_LAB_VALID = ("[\\s]*(LAB|TUT)[\\s]*");
 //    String TUT_LAB_NUMBER = ("[\\s]*([\\d]+)[\\s]*");
-//    String COMMA = ("[\\s]*,[\\s]*");
+//    String COMMA = ("([\\s]*)[\\s])*");
 
-    public static final String COURSE_VALID_DAY = ("[\\s*](MO|TU)[\\s]*");
-    public static final String LAB_VALID_DAY = ("[\\s*](MO|TU|FR)([\\s]*)");
-    public static final String TIMES = "[\\s*](((([0|1]?)[\\d])|([2][0-3])):([0-5][\\d]))[\\s]*";
+    public static final String COURSE_VALID_DAY = ("([\\s]*(MO|TU)[\\s]*)");
+    public static final String LAB_VALID_DAY = ("([\\s]*(MO|TU|FR)([\\s]*))");
+    public static final String TIMES = "([\\s]*(((([0|1]?)[\\d])|([2][0-3])):([0-5][\\d]))[\\s]*)";
     public static final String COURSE_SLOTS = ("([\\s]*(MO|TU)[\\s]*,[\\s]*(([0|1]?[\\d]|[2][0-3]):([0-5][\\d]))[\\s]*,[\\s]*([\\d]+)[\\s]*,[\\s]*([\\d]+)[\\s]*)");
     public static final String LAB_SLOTS = ("([\\s]*(MO|TU|FR)[\\s]*,[\\s]*(([0|1]?[\\d]|[2][0-3]):([0-5][\\d]))[\\s]*,[\\s]*([\\d]+)[\\s]*,[\\s]*([\\d]+)[\\s]*)");
     public static final String COURSES = ("([\\s]*(CPSC|SENG)[\\s]*([\\d]{3})[\\s]*(LEC)[\\s]*([\\d]{2})[\\s]*)");
     public static final String LABS = ("([\\s]*(CPSC|SENG)[\\s]*([\\d]{3})[\\s]*(LEC)[\\s]*([\\d]{2})[\\s]*(TUT|LAB)[\\s]*([\\d]{2})[\\s]*)");
-    public static final String TUT = ("(([\\s]*(CPSC|SENG)[\\s]*([\\d]{3})[\\s]*(TUT|LAB)[\\s]*([\\d]{2}))[\\s]*)");
+    public static final String TUT = "(([\\s]*(CPSC|SENG)[\\s]*([\\d]{3})[\\s]*(TUT)[\\s]*([\\d]{2}))[\\s]*)";
 
-    public static final String NOTCOMPATABLE = COURSES + "|" + LABS + "," + COURSES + "|" + LABS;
+    public static final String NOTCOMPATABLE_FORMAT = "("+LABS + "|" + TUT + "|" + COURSES +")" + "," + "(" + LABS + "|" + TUT + "|" + COURSES + ")";
+    public static final String UNWANTED = "("+LABS +"|"+TUT+ "|" + COURSES +")" + "," + "("+ LAB_VALID_DAY +"|"+ COURSE_VALID_DAY +")"+  "," + TIMES;
     public static final String UNWANTED_C = COURSES + "," + COURSE_VALID_DAY +  "," + TIMES;
     public static final String UNWANTED_L = LABS + "," + LAB_VALID_DAY  + "," + TIMES;
-    public static final String PREFERENCES_C = COURSE_VALID_DAY + "," + TIMES + "," + COURSES + "[\\s]*([\\d]+)[\\s]*";
-    public static final String PREFERENCES_L = COURSE_VALID_DAY + "," + TIMES + "," + COURSES + "[\\s]*([\\d]+)[\\s]*";
-    public static final String PAIR = NOTCOMPATABLE;
-    public static final String PARTIALASSIGNMENT = UNWANTED_C;
+    public static final String UNWANTED_T = TUT + "," + LAB_VALID_DAY  + "," + TIMES;
+
+    public static final String PREFERENCES =  LAB_VALID_DAY + "," + TIMES + "," + "("+LABS + "|" + TUT + "|" + COURSES +")" +","+ "([\\s]*([\\d]+)[\\s]*)*";
+    public static final String PREFERENCES_C = COURSE_VALID_DAY + "," + TIMES + "," + COURSES +","+ "([\\s]*([\\d]+)[\\s]*)";
+    public static final String PREFERENCES_L = LAB_VALID_DAY + "," + TIMES + "," + LABS + ","+"([\\s]*([\\d]+)[\\s]*)";
+    public static final String PREFERENCES_T = LAB_VALID_DAY + "," + TIMES + "," + TUT + ","+ "([\\s]*([\\d]+)[\\s]*)";
+    public static final String PAIR = NOTCOMPATABLE_FORMAT;
+    public static final String PARTIALASSIGNMENT = UNWANTED;
 
 //    public static void main(String args[]){
 //        String line = ("CPSC 433 LEC 01 TUT 01, CPSC 433 LEC 02 LAB 02");
-////        if (line.matches(NOTCOMPATABLE)){
+//        if (line.matches(NOTCOMPATABLE)){
 //            System.out.println(NOTCOMPATABLE);
-////        }
+//        }
 
 }
 
