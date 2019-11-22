7ffacd26f9d0805cae6c1b69cd81901035ee1250
Zahra <zahra.ghavasieh@ucalgary.ca>
2019-11-22 12:43:53 -0700

    idk

--- src/main/java/Eval.java
+++ src/main/java/Eval.java
@@ -1,8 +1,10 @@
 import DataClass.Assignment;
 import DataClass.PSol;
+import DataClass.PreferredCoursePair;
+import DataClass.Slot;
 
 /* Assignment attributes:
-preference_value of each assignment in assign.
+preference_value of each assignment in assign. (courses + Labs) x Slots -> Int
 */
 
 
@@ -23,29 +25,47 @@ preference-value. function: (courses + Labs) x Slots -> Int
 public class Eval {
 
     //Attributes
-    int pen_coursemin;              //for each course below coursemin
-    int pen_labsmin;                //for each lab below labmin
-    int pen_notpaired;              //for each pair(a,b) for which assign(a) != assign(b)
-    int pen_selection;              //for each pair of sections that is schedule into the same slot
+    private int pen_coursemin;              //for each course below coursemin
+    private int pen_labsmin;                //for each lab below labmin
+    private int pen_notpaired;              //for each pair(a,b) for which assign(a) != assign(b)
+    private int pen_section;                //for each pair of sections that is schedule into the same slot
+    private PreferredCoursePair[] pairs;
 
     //Constructors
     public Eval(){
         pen_coursemin = 0;
         pen_labsmin = 0;
         pen_notpaired = 0;
-        pen_selection = 0;
+        pen_section = 0;
+        pairs = new PreferredCoursePair[0];
     }
 
-    public Eval(int pen_coursemin, int pen_labsmin, int pen_notpaired, int pen_selection){
+    public Eval(int pen_coursemin, int pen_labsmin, int pen_notpaired, int pen_section, PreferredCoursePair[] pairs){
         this.pen_coursemin = pen_coursemin;
         this.pen_labsmin = pen_labsmin;
         this.pen_notpaired = pen_notpaired;
-        this.pen_selection = pen_selection;
+        this.pen_section = pen_section;
+        this.pairs = pairs;
     }
 
     //Functions
-    public int eval(PSol pr){
+    public int eval(PSol sol, PreferredCoursePair[] pairs){
 
+        for (Slot slot : sol.getLinked().keySet()) {
+            if (slot == null) {
+                continue;
+            }
+            //add up number of courses assigned to the same slot
+            //add up number of labs assigned to the same slot
+            //
+        }
+        return 0;
+    }
+
+    public int evalPartial(PSol sol){
+        for (Slot slot : sol.getLinked().keySet()){
+
+        }
         return 0;
     }
 
--- src/main/kotlin/DataClass/PSol.kt
+++ src/main/kotlin/DataClass/PSol.kt
@@ -3,9 +3,12 @@ package DataClass
 // makes the class struct-like.
 data class PSol(val assignments: List<Assignment>) {
 
-    // creates ocerloaded function so java coded can use the default parameters.
+    // creates overloaded function so java coded can use the default parameters.
     @JvmOverloads
     // creates new copy of the pSol that has one item reassigned.
     // .minus().plus() creates new copy with 1 element replaced.
     fun copyEdit(new: Assignment, original: Assignment = Assignment(new.course, null)) : PSol = PSol(assignments.minus(original).plus(new))
+
+
+    var linked : HashMap<Slot?, List<Course>> = HashMap()
 }
