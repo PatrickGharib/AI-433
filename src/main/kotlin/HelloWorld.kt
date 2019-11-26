0a6ba4725ae9d6932d1a9f393fc9ef70185e7f8f
Devon Hockley <devon@hockley.ca>
2019-11-26 14:53:52 -0700

    made preliminary executable version

--- src/main/kotlin/HelloWorld.kt
+++ src/main/kotlin/HelloWorld.kt
@@ -1,4 +1,29 @@
+import DataClass.Assignment
+import DataClass.CourseSlot
+import DataClass.PSol
+import IO.ParsedData
+import IO.Parser
+import java.nio.file.Path
+import java.nio.file.Paths
 
-fun main(args: Array<String>){
-    println("Hello world")
+fun main() {
+    Parser.inputReader("deptinst1.txt")
+    print(Paths.get("").toAbsolutePath())
+    CourseSchedulerProcess(constructPSol()).execute()
+}
+
+fun constructPSol() : PSol{
+    val x: MutableList<Assignment> = mutableListOf()
+    ParsedData.PARTIAL_ASSIGNMENTS.forEach {
+        x.add(Assignment(it.course, it.slot as CourseSlot?))
+    }
+    val exclude = ParsedData.PARTIAL_ASSIGNMENTS.map { it.course }
+    ParsedData.COURSES.forEach {
+        if (it in exclude){
+            return@forEach
+        }else {
+            x.add(Assignment(it, null))
+        }
+    }
+    return PSol(x)
 }
