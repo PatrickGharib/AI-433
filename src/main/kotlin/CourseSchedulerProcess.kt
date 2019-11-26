79aa15d236aa633ef5e27030a9f34c57cb9d35b7
Devon Hockley <devon@hockley.ca>
2019-11-26 13:03:26 -0700

    some logical fixes and optimization for edge cases

--- src/main/kotlin/CourseSchedulerProcess.kt
+++ src/main/kotlin/CourseSchedulerProcess.kt
@@ -12,18 +12,26 @@ class CourseSchedulerProcess(root: PSol): SearchProcess<CourseSchedulerTree, PSo
         node!!.expand()
         if (node.children.isEmpty()){
             node.solved = true
-            return
+        }
+
+        // candidate?.value ?: 100000 explanation:
+        // candidate?.value says IF candidate != null then get its value, otherwise return null, (no null pointer exception)
+        // then ?: says if the left is null then return the right, in this case 100000.
+        // so if candidate is null it goes: (candidate?.value) ?: 100000 -> (null) ?: 100000 -> 100000
+
+        if (node.solved && node.data.complete && node.data.value < (candidate?.value ?: 100000)){
+            candidate = node.data
         }
         node.children.forEach {
             it.solved = Solved(it)
-            if (it.solved &&  (it.data.value > candidate?.value ?: 100000)){
+            if (it.solved && it.data.complete && (it.data.value < (candidate?.value ?: 100000))){
                 candidate = it.data
             }
         }
     }
 
     private fun Solved(it: AndTree<PSol>.Node): Boolean {
-        //only need to determine if soloution is complete
+        //only need to determine if solution is complete
         return it.data.complete
     }
 
