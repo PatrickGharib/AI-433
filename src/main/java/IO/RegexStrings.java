cf1a84ba8ed5b383288009d0ecc8bab9d44ccffd
PatrickGharib <patrick.a.gharib@gmail.com>
2019-12-04 17:29:32 -0700

    fixed regex

--- src/main/java/IO/RegexStrings.java
+++ src/main/java/IO/RegexStrings.java
@@ -29,8 +29,8 @@ public class RegexStrings {
 
     public static final String UNWANTED ="("+ LABS +"|"+TUT+ "|" + COURSES +")" +"," + "("+LAB_VALID_DAY +"|"+ COURSE_VALID_DAY+")" + "," + TIMES;
     public static final String CRS_DAY_TIME = "[\\s]*(CPSC|SENG)[\\s]*([\\d]{3})[\\s]*(LEC)[\\s]*([\\d]{2})[\\s]*,[\\s]*(MO|TU)[\\s]*,[\\s]*([0|1]?[\\d]|[2][0-3]):([0-5][\\d])[\\s]*";
-    public static final String LAB_DAY_TIME ="[\\s]*(CPSC|SENG)[\\s]*([\\d]{3})[\\s]*(LEC)[\\s]*([\\d]{2})[\\s]*(TUT|LAB)[\\s]*([\\d]{2})[\\s]*,[\\s]*(MO|TU)[\\s]*,[\\s]*([0|1]?[\\d]|[2][0-3]):([0-5][\\d])[\\s]*";
-    public static final String TUT_DAY_TIME = "[\\s]*(CPSC|SENG)[\\s]*([\\d]{3})[\\s]*(TUT|LAB)[\\s]*([\\d]{2})[\\s]*,[\\s]*(MO|TU)[\\s]*,[\\s]*([0|1]?[\\d]|[2][0-3]):([0-5][\\d])[\\s]*";
+    public static final String LAB_DAY_TIME ="[\\s]*(CPSC|SENG)[\\s]*([\\d]{3})[\\s]*(LEC)[\\s]*([\\d]{2})[\\s]*(TUT|LAB)[\\s]*([\\d]{2})[\\s]*,[\\s]*(MO|TU|FR)[\\s]*,[\\s]*([0|1]?[\\d]|[2][0-3]):([0-5][\\d])[\\s]*";
+    public static final String TUT_DAY_TIME = "[\\s]*(CPSC|SENG)[\\s]*([\\d]{3})[\\s]*(TUT|LAB)[\\s]*([\\d]{2})[\\s]*,[\\s]*(MO|TU|FR)[\\s]*,[\\s]*([0|1]?[\\d]|[2][0-3]):([0-5][\\d])[\\s]*";
 
     public static final String PREFERENCES =  LAB_VALID_DAY + "," + TIMES + "," +"("+LABS + "|" + TUT + "|" + COURSES+ ")"+","+ "[\\s]*([\\d]+)[\\s]*";
     public static final String PREFERENCES_C = COURSE_VALID_DAY + "," + TIMES + "," + COURSES +","+ "[\\s]*([\\d]+)[\\s]*";
